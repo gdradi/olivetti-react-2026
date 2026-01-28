2e57e7b88d5a218bee4f590aa7b28f19210e91a9
Giacomo Dradi <giacomo.dradi@vem.com>
2026-01-28 10:36:43 +0100

    Lezione 6 - todoapp corretta

--- src/todoapp.tsx
+++ src/todoapp.tsx
@@ -6,6 +6,7 @@ interface Task {
 
 export function TodoApp() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
 
   return (
     <div className="box">
@@ -13,6 +14,21 @@ export function TodoApp() {
       {tasks.map((task, index) => (
         <div key={index}>
           {index + 1}. {task.text}
+          <button
+            onClick={() => {
+              const tasksWithoutThisTask = tasks.filter((task, i) => i !== index);
+              setTasks(tasksWithoutThisTask);
+            }}
+          >
+            Elimina
+          </button>
+          <button
+            onClick={() => {
+              setEditingIndex(index);
+            }}
+          >
+            Modifica
+          </button>
         </div>
       ))}
       <CreateTask
@@ -21,6 +37,16 @@ export function TodoApp() {
           setTasks(newList);
         }}
       />
+      {editingIndex !== null && (
+        <UpdateTask
+          task={tasks[editingIndex]!}
+          onTaskUpdated={(updatedTask) => {
+            const updatedTasks = tasks.map((task, index) => (index === editingIndex ? updatedTask : task));
+            setTasks(updatedTasks);
+            setEditingIndex(null);
+          }}
+        />
+      )}
     </div>
   );
 }
@@ -34,16 +60,7 @@ function CreateTask(props: CreateTaskProps) {
   return (
     <div className="box">
       <div style={{ fontWeight: "bold" }}>CreateTask</div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyDown={(ev) => {
-          if (ev.code === "Enter") {
-            // TODO
-          }
-        }}
-      />
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
       <button
         onClick={() => {
           const newTask: Task = { text: text };
@@ -52,6 +69,30 @@ function CreateTask(props: CreateTaskProps) {
         }}
       >
         Create
+      </button>
+    </div>
+  );
+}
+
+interface EditTaskProps {
+  readonly task: Task;
+  readonly onTaskUpdated: (updatedTask: Task) => void;
+}
+function UpdateTask(props: EditTaskProps) {
+  const { task, onTaskUpdated } = props;
+  const [text, setText] = useState(task.text);
+  return (
+    <div className="box">
+      <div style={{ fontWeight: "bold" }}>UpdateTask</div>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      <button
+        onClick={() => {
+          const newTask: Task = { text: text };
+          onTaskUpdated(newTask);
+          setText("");
+        }}
+      >
+        Update
       </button>
     </div>
   );
